ede1a3640afcefffee7f2e177e5812d3eb27238b
Oleg Kulikov <oakulikov@yandex.ru>
2016-11-11 18:28:01 +0300

    done 4

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,70 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+  @16384
+  D=A
+  @R1
+  M=D
+  @R0
+  M=0
+(INPUT)
+  @KBD
+  D=M
+  @FILL0
+  D;JEQ
+  @FILL1
+  0;JMP
+(FILL0)
+  @R0
+  D=M
+  @FILL000
+  D;JEQ
+(FILL00)
+  @16384
+  D=A
+  @R1
+  M=D
+(FILL000)
+  @R0
+  M=0
+  @24575
+  D=A
+  @R1
+  D=D-M
+  @INPUT
+  D;JLT
+  @R1
+  D=M
+  A=D
+  M=0
+  @R1
+  M=M+1
+  @INPUT
+  0;JMP
+(FILL1)
+  @R0
+  D=M
+  @FILL111
+  D;JGT
+(FILL11)
+  @16384
+  D=A
+  @R1
+  M=D
+(FILL111)
+  @R0
+  M=1
+  @24575
+  D=A
+  @R1
+  D=D-M
+  @INPUT
+  D;JLT
+  @R1
+  D=M
+  A=D
+  M=-1
+  @R1
+  M=M+1
+  @INPUT
+  0;JMP
